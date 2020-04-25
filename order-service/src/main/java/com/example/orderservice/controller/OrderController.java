6a4e810bd2f3006b0be11327cb8344ae271111eb
Pedro Ramos <ramuspedro@gmail.com>
2020-04-25 19:49:29 -0300

    request to server

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,10 +1,12 @@
 package com.example.orderservice.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:33493")
 @RequestMapping(value = "/order")
 public class OrderController {
     @RequestMapping(value = "", method = RequestMethod.POST)
--- user_app/lib/main.dart
+++ user_app/lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
               print("Response body: ${response.contentLength}");
               print(response.headers);
               print(response.request);
+              print(response.body);
             });
           },
           child: Text(
